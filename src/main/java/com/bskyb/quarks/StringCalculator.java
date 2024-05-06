d4e7f4c9bb23a799694546c1a00c7bb1507d4e09
Pacareu, Marc <marc.pacareu@sky.uk>
2024-05-06 13:31:46 +0100

    First commit

---
+++ src/main/java/com/bskyb/quarks/StringCalculator.java
@@ -0,0 +1,11 @@
+package com.bskyb.quarks;
+
+/**
+ * Hello world!
+ */
+public class StringCalculator {
+    public int add(String numbers) {
+        //TODO: implement something :)
+        return 42;
+    }
+}
---
+++ src/test/java/com/bskyb/quarks/StringCalculatorTest.java
@@ -0,0 +1,13 @@
+package com.bskyb.quarks;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class StringCalculatorTest {
+
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+}
