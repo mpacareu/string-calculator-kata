a1a1227e883c1acd7431615df897a465e0d5f5e2
aro994 <arnau.roca@sky.uk>
2025-03-14 14:43:21 +0000

    Finish (I think)

--- src/main/java/com/bskyb/quarks/StringCalculator.java
+++ src/main/java/com/bskyb/quarks/StringCalculator.java
@@ -1,11 +1,110 @@
 package com.bskyb.quarks;
 
-/**
- * Hello world!
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringCalculator {
-    public int add(String numbers) {
-        //TODO: implement something :)
-        return 42;
+
+    public static final String DELIMITER_STARTER = "//";
+    public static final String COMA = ",";
+    public static final String NULL_STRING = "";
+    public static final String LINE_BREAK = "\n";
+
+    public static final int ZERO = 0;
+    public static final int ONE = 1;
+    public static final int TWO = 2;
+    public static final int THREE = 3;
+
+    public static final String OPENING_SQUARE_BRACKET = "\\[";
+    public static final String OPENING_SQUARE_BRAKET_BASIC = "[";
+    public static final String CLOSING_SQUARE_BRACKET = String.valueOf(']');
+    public static final String OPENING_CURLY_BRACES = "{";
+    public static final String CLOSING_CURLY_BRACES = "}";
+    public static final String CLOSING_BRAKET = "\\)";
+    public static final String OPENING_BRAKET = "\\(";
+
+    public static final String ASTERISC = "\\*";
+    public static final String SUM = "\\+";
+
+    public static final String NEGATIVE_NUMBERS_ARE_NOT_SUPPORTED = "Negative numbers are not supported";
+
+    public Integer add(String numbers) {
+        List<Integer> negatives = new ArrayList<>();
+        List<String> delimiters = new ArrayList<>();
+
+        //should substitute this for something taking care of all special characters
+        numbers = dealWithMetacharacters(numbers);
+        if (numbers.startsWith(DELIMITER_STARTER)) {
+            if (isMoreThanOneDelimiterOrLongDelimiter(numbers)) {
+                numbers = numbers.replaceFirst(DELIMITER_STARTER, NULL_STRING);
+                while (numbers.startsWith(OPENING_SQUARE_BRAKET_BASIC)) {
+                    delimiters.add(getLongDelimiter(numbers));
+                    numbers = numbers.replaceFirst(OPENING_SQUARE_BRACKET + delimiters.get(delimiters.size() - ONE) + CLOSING_SQUARE_BRACKET, NULL_STRING);
+                }
+            } else {
+                delimiters.add(getShortDelimiter(numbers));
+                numbers = numbers.replaceFirst(DELIMITER_STARTER + delimiters, NULL_STRING);
+            }
+        }
+
+        String[] numbersArray = getArrayOfNumbersSepared(numbers, delimiters);
+
+        int sum = ZERO;
+        for (String number : numbersArray) {
+            if (isNumberLowerThan1000(number)) {
+                int parsedNumber = Integer.parseInt(number);
+                if (isNegative(parsedNumber)) {
+                    negatives.add(parsedNumber);
+                } else {
+                    sum += parsedNumber;
+                }
+            }
+        }
+        if (!negatives.isEmpty()) {
+            throw new IllegalArgumentException(NEGATIVE_NUMBERS_ARE_NOT_SUPPORTED);
+        }
+        return sum;
+    }
+
+    private static String[] getArrayOfNumbersSepared(String numbers, List<String> delimiters) {
+        numbers = dealWithUnclosedCharacter(numbers.replace(LINE_BREAK, COMA));
+        for (String delimiter : delimiters) {
+            numbers = numbers.replace(delimiter,COMA);
+        }
+        return numbers.split(COMA);
+    }
+
+    private static String dealWithUnclosedCharacter(String numbers) {
+        if(numbers.contains(CLOSING_BRAKET)||numbers.contains(OPENING_BRAKET)||numbers.contains(OPENING_CURLY_BRACES)|| numbers.contains(CLOSING_CURLY_BRACES) || numbers.contains(CLOSING_SQUARE_BRACKET) || numbers.contains(OPENING_SQUARE_BRACKET)){
+            numbers = numbers.replace(CLOSING_BRAKET,NULL_STRING).replace(OPENING_BRAKET,NULL_STRING).replace(OPENING_CURLY_BRACES,COMA).replace(CLOSING_CURLY_BRACES,COMA).replace(CLOSING_SQUARE_BRACKET,COMA).replace(OPENING_SQUARE_BRACKET,COMA);
+        }
+        return numbers;
+    }
+
+    private String dealWithMetacharacters(String numbers) {
+        return numbers.replaceAll(ASTERISC, COMA).replaceAll(SUM, COMA);
+    }
+
+    private static boolean isNegative(int parsedNumber) {
+        return parsedNumber < ZERO;
+    }
+
+    private static boolean isNumberLowerThan1000(String number) {
+        return !(NULL_STRING.equals(number) || number.length() > THREE);
+    }
+
+    private static boolean isMoreThanOneDelimiterOrLongDelimiter(String numbers) {
+        return OPENING_SQUARE_BRAKET_BASIC.equals(Character.toString(numbers.charAt(TWO))) && !(LINE_BREAK.equals(Character.toString(numbers.charAt(THREE))));
+    }
+
+    private static String getLongDelimiter(String numbers) {
+        String delimiter;
+        String[] numbersArrays = numbers.split(CLOSING_SQUARE_BRACKET);
+        delimiter = numbersArrays[ZERO].replaceFirst(OPENING_SQUARE_BRACKET, NULL_STRING).replace(CLOSING_SQUARE_BRACKET, NULL_STRING);
+        return delimiter;
+    }
+
+    private static String getShortDelimiter(String numbers) {
+        return String.valueOf(numbers.charAt(TWO));
     }
 }
--- src/test/java/com/bskyb/quarks/StringCalculatorTest.java
+++ src/test/java/com/bskyb/quarks/StringCalculatorTest.java
@@ -2,12 +2,74 @@ package com.bskyb.quarks;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class StringCalculatorTest {
 
     @Test
-    void test() {
-        assertTrue(true);
+    void addIf0() {
+        StringCalculator calculator = new StringCalculator();
+
+        assertEquals(0, calculator.add(""), "Not returning 0 if empty string");
+    }
+
+    @Test
+    void addNumbersSeparatedByComma() {
+        StringCalculator calculator = new StringCalculator();
+
+        assertEquals(4, calculator.add("4"), "Not returning the correct number");
+        assertEquals(4, calculator.add("3,,1"), "Not returning the correct number");
+        assertEquals(119, calculator.add("20,45,54"), "Not returning the correct number");
+    }
+
+    @Test
+    void addNumbersSeparatedByNewLines() {
+        StringCalculator calculator = new StringCalculator();
+
+        assertEquals(6, calculator.add("1\n2,3"), "Not returning the correct number");
+        assertEquals(6, calculator.add("1\n2,\n\n3"), "Not returning the correct number");
+    }
+
+    @Test
+    void addWithDifferentDelimiters() {
+        StringCalculator calculator = new StringCalculator();
+
+        assertEquals(3, calculator.add("//;\n1;2"), "Not returning the correct number");
+        assertEquals(3, calculator.add("// \n1 2"), "Not returning the correct number");
+        assertEquals(3, calculator.add("///\n1/2"), "Not returning the correct number");
+        assertEquals(3, calculator.add("//}\n1{2"), "Not returning the correct number");
+    }
+
+    @Test
+    void throwExceptionWhenIsNegative() {
+        StringCalculator calculator = new StringCalculator();
+        assertThrows(IllegalArgumentException.class, () -> calculator.add("-1"));
+
+    }
+
+    @Test
+    void numbersBiggerThan1000ShouldBeIgnored() {
+        StringCalculator calculator = new StringCalculator();
+
+        assertEquals(2, calculator.add("1001,2"), "Exception not thrown");
+        assertEquals(1006, calculator.add("1000,2,5,999"), "Exception not thrown");
+    }
+
+    @Test
+    void addWhenDelimiterItsMoreTHanOneCharacter() {
+        StringCalculator calculator = new StringCalculator();
+
+        assertEquals(6, calculator.add("//[´´´]\n1´´´2´´´3"), "Not returning the correct number");
+        assertEquals(6, calculator.add("//[***]\n1***2***3"), "Not returning the correct number");
+    }
+
+    @Test
+    void addWhenMoreThanOneDelimiter() {
+        StringCalculator calculator = new StringCalculator();
+
+        assertEquals(6, calculator.add("//[/][;]\n1/2;3"), "Exception not thrown");
+        assertEquals(6, calculator.add("//[///][;´]\n1///2;´3"), "Exception not thrown");
+        assertEquals(10, calculator.add("//[///][;´][_]\n1///2;´3_4"), "Exception not thrown");
     }
 }
