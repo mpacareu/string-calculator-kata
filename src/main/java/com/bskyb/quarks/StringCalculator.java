24d81243300b7e2d5417d0e3690f4951e0197cc9
Casas, Arnau <arnau.casas@sky.uk>
2026-05-13 15:11:13 +0100

    Function added and test created.

--- src/main/java/com/bskyb/quarks/StringCalculator.java
+++ src/main/java/com/bskyb/quarks/StringCalculator.java
@@ -4,8 +4,52 @@ package com.bskyb.quarks;
  * Hello world!
  */
 public class StringCalculator {
+
     public int add(String numbers) {
-        //TODO: implement something :)
-        return 42;
+        int result = 0;
+        String delimiters = "[,\\n]";
+        String stringNumbers = numbers;
+
+        boolean exception = false;
+        StringBuilder exceptionMessage = new StringBuilder("negatives not allowed:");
+
+        if(!numbers.isEmpty()){
+            //Assignation of delimiters
+            if(numbers.startsWith("//")){
+                int index = numbers.indexOf("\n");
+                var tempDelimiters = numbers.substring(2, index).split(" ");
+                if(tempDelimiters.length>1){
+                    StringBuilder temp = new StringBuilder();
+                    for(String delimiter : tempDelimiters){
+                        int indexDelimiter = delimiter.indexOf("]");
+                        temp.append(delimiter, 1, indexDelimiter).append("\\");
+                    }
+                    temp.insert(0, "[");
+                    temp.setCharAt(temp.length()-1, ']');
+                    delimiters = temp.toString();
+                }else{
+                    delimiters = tempDelimiters[0];
+                }
+                stringNumbers = stringNumbers.replaceFirst("//.+\\n", "");
+            }
+
+            //Read list of numbers and calculate
+            var listNum = stringNumbers.split(delimiters);
+            for(String num : listNum){
+                if(!num.isEmpty()){
+                    var numero = Integer.parseInt(num);
+                    if(numero < 0){
+                        exception = true;
+                        exceptionMessage.append(" ").append(numero);
+                    }else{
+                        if(numero <= 1000) result += numero;
+                    }
+                }
+            }
+            if(exception){
+                throw new IllegalArgumentException(exceptionMessage.toString());
+            }
+        }
+        return result;
     }
 }
--- src/test/java/com/bskyb/quarks/StringCalculatorTest.java
+++ src/test/java/com/bskyb/quarks/StringCalculatorTest.java
@@ -2,12 +2,58 @@ package com.bskyb.quarks;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorTest {
+    StringCalculator calcluator = new StringCalculator();
+    @Test
+    void emptyStringTest() {
+        assertEquals(0, calcluator.add(""));
+    }
 
     @Test
-    void test() {
-        assertTrue(true);
+    void singularNumberTest() {
+        assertEquals(1, calcluator.add("1"));
+        assertEquals(2, calcluator.add("2"));
+        assertEquals(3, calcluator.add("3"));
+    }
+
+    @Test
+    void separatedByComaTest() {
+        assertEquals(3, calcluator.add("1,2"));
+        assertEquals(12, calcluator.add("1,2,3,6"));
+    }
+
+    @Test
+    void separatedByIntroTest() {
+        assertEquals(3, calcluator.add("1\n2"));
+        assertEquals(6, calcluator.add("1\n2,3"));
+        assertEquals(5, calcluator.add("\n2,3"));
+    }
+
+    @Test
+    void delimeterConditionTest(){
+        assertEquals(3,calcluator.add("//;\n1;2"));
+    }
+
+    @Test
+    void negativeNumberConditionTest(){
+        assertThrowsExactly(IllegalArgumentException.class, () -> calcluator.add("1,2,-3"));
+        assertThrowsExactly(IllegalArgumentException.class, () -> calcluator.add("-1,-2,-3"));
+    }
+
+    @Test
+    void moreThanOneThousandTest(){
+        assertEquals(1003,calcluator.add("1,2,1000"));
+        assertEquals(3,calcluator.add("1,2,1001"));
+    }
+    @Test
+    void longDelimetersTest(){
+        assertEquals(6,calcluator.add("//[***]\n1***2***3"));
+    }
+
+    @Test
+    void diverseDelimetersTest(){
+        assertEquals(24,calcluator.add("//[***] [@] [??]\n1***2***3??6@12"));
     }
 }
